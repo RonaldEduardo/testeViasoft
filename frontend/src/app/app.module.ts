f7aad7c4f35939e3d18a731b3932b7a285b27063
ronaldgustmann <ronald29edruardo@gmail.com>
2026-03-22 16:09:16 -0300

    feat(frontend): adiciona cabecalho global com navegacao

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductModule } from './features/product/product.module';
 import { HttpClientModule } from '@angular/common/http';
+import { HeaderComponent } from './shared/components/header/header.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HeaderComponent],
   imports: [BrowserModule, AppRoutingModule, ProductModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
