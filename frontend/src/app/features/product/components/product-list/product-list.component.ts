cad51dd496f28db711384f422f49a7d46e2ead28
ronaldgustmann <ronald29edruardo@gmail.com>
2026-03-22 16:09:42 -0300

    refact(frontend): remove styleUrls nao utilizado da listagem de produtos

--- frontend/src/app/features/product/components/product-list/product-list.component.ts
+++ frontend/src/app/features/product/components/product-list/product-list.component.ts
@@ -5,7 +5,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.scss'],
 })
 export class ProductListComponent implements OnInit {
   products: Product[] = [];
